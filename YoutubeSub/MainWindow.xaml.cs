263d8c27e01e48dba92fdc2e50bb5bed3ddde2d5
Anatoliy Novikov <chamskra66er@gmail.com>
2020-09-04 13:53:20 +0300

    added lang buttons

--- YoutubeSub/MainWindow.xaml.cs
+++ YoutubeSub/MainWindow.xaml.cs
@@ -50,5 +50,11 @@ namespace YoutubeSub
                 this.DragMove();
             }
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (LeftGrid.Visibility == Visibility.Hidden) LeftGrid.Visibility = Visibility.Visible;
+            else LeftGrid.Visibility = Visibility.Hidden;
+        }
     }
 }
